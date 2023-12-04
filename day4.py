b05ec538dced4f0ec0f8907db8d57226358ad93d
guohaoice <guohaoice@gmail.com>
2023-12-04 13:23:51 +0800

    day4

--- day4.py
+++ day4.py
@@ -1,6 +1,13 @@
+import re
 from typing import List
 
 from day import Day
+
+
+def count_winning(line: str) -> int:
+    n = re.findall(r'\d+', line.split('|')[0].split(':')[1])
+    m = re.findall(r'\d+', line.split('|')[1])
+    return sum(x in n for x in m)
 
 
 class Day4(Day):
@@ -9,16 +16,15 @@ class Day4(Day):
         super().__init__(day=4)
 
     def part_one(self, lines: List[str]) -> int:
-        ret = 0
-        for i in range(len(lines)):
-            pass
-        return ret
+        return sum([int(2 ** (count_winning(line) - 1)) for line in lines])
 
     def part_two(self, lines: List[str]) -> int:
-        ret = 0
+        d = {x: 1 for x in range(len(lines))}
         for i in range(len(lines)):
-            pass
-        return ret
+            wining = count_winning(lines[i])
+            for j in range(i + 1, wining + i + 1):
+                d[j] = d[j] + d[i]
+        return sum(d.values())
 
 
 if __name__ == '__main__':
