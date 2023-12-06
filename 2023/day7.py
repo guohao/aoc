34eaffd88da50581b453dd3c6878b1000b8f316e
guohaoice <guohaoice@gmail.com>
2023-12-06 22:14:37 +0800

    add helper funciton

--- 2023/day7.py
+++ 2023/day7.py
@@ -4,4 +4,5 @@ data = """
 # data = io_utils.get_data(2023, 7)
 # data = io_utils.raw_str_to_lines(data)
 # parts = data.split('\n\n')
+# ints = [int(x) for x in re.findall(r'\d+',line)]
 # re.findall(r'\d+', data[0])
