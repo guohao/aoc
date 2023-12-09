018a467be4ed6ec6f7d08d6d79ee3ac160e63c92
guohaoice <guohaoice@gmail.com>
2023-12-10 07:56:21 +0800

    2022 day10

---
+++ 2022/day10.py
@@ -0,0 +1,27 @@
+from common import io_utils
+
+data = io_utils.get_data(2022, 10)
+lines = data.strip().splitlines()
+x = 1
+s = []
+
+for line in lines:
+    s.append(x)
+    c = line.split()[0]
+    if c.startswith('add'):
+        v = int(line.split()[1])
+        s.append(x)
+        x += v
+
+t = [20, 60, 100, 140, 180, 220]
+print(sum([x * s[x - 1] for x in t]))
+
+o = ''
+for i in range(0, len(s)):
+    x = s[i]
+    if x - 1 <= i % 40 <= x + 1:
+        o += '#'
+    else:
+        o += '.'
+for i in range(0, len(o), 40):
+    print(o[i:i + 40])
