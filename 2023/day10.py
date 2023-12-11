fc1ba8bf1335c67f42bf4c5d15e269cecd0491d1
guohaoice <guohaoice@gmail.com>
2023-12-11 20:30:28 +0800

    day 10 and day 11

--- 2023/day10.py
+++ 2023/day10.py
@@ -1,144 +1,49 @@
+import itertools
+
 from common import io_utils
 
-data = """
-...........
-.S-------7.
-.|F-----7|.
-.||.....||.
-.||.....||.
-.|L-7.F-J|.
-.|..|.|..|.
-.L--J.L--J.
-...........
-"""
-data ="""
-FF7FSF7F7F7F7F7F---7
-L|LJ||||||||||||F--J
-FL-7LJLJ||||||LJL-77
-F--JF--7||LJLJ7F7FJ-
-L---JF-JLJ.||-FJLJJ7
-|F|F-JF---7F7-L7L|7|
-|FFJF7L7F-JF7|JL---7
-7-L-JL7||F7|L7F-7F7|
-L.L7LFJ|||||FJL7||LJ
-L7JLJL-JLJLJL--JLJ.L
-"""
+data = io_utils.get_data(2023, 10)
+P = {
+    'S': [(0, 1), (-1, 0), (1, 0), (0, -1)],
+    '-': [(0, -1), (0, 1)],
+    '|': [(-1, 0), (1, 0)],
+    'F': [(0, 1), (1, 0)],
+    'L': [(0, 1), (-1, 0)],
+    'J': [(0, -1), (-1, 0)],
+    '7': [(0, -1), (1, 0)],
+    '.': []}
 
-# data = io_utils.get_data(2023, 10)
 lines = io_utils.raw_str_to_lines(data)
-grid = []
-d = []
-for line in lines:
-    grid.append([x for x in line])
-    d.append([0 for x in line])
+grid = {(i, j): lines[i][j] for i, j in itertools.product(range(len(lines)), range(len(lines[0])))}
 
-S = (-1, -1)
-for i, line in enumerate(grid):
-    for j, y in enumerate(line):
-        if y == 'S':
-            S = (i, j)
-            break
-f = 'U'
-v = set()
-v.add(S)
-cp = S
-idx = [(S[0] - 1, S[1]), (S[0], S[1] + 1), (S[0] + 1, S[1]), (S[0], S[1] - 1)]
-for i in idx:
-    if grid[i[0]][i[1]] != '.':
-        cp = i
+
+def t(p):
+    if p not in grid:
+        return []
+    return [(x[0] + p[0], x[1] + p[1]) for x in P[grid[p]]]
+
+
+S = [p for p in grid.keys() if grid[p] == 'S'][0]
+nv = [n for n in t(S) if S in t(n)]
+diff = {(nv[0][0] - S[0], nv[0][1] - S[1]), (nv[1][0] - S[0], nv[1][1] - S[1])}
+assert len(nv) == 2
+grid[S] = [i[0] for i in P.items() if diff == set(i[1])][0]
+
+pipes = []
+nv = S
+while nv:
+    pipes.append(nv)
+    nv = [n for n in t(nv) if n not in pipes]
+    if len(nv) == 0:
         break
+    nv = nv[0]
+print(len(pipes) // 2)
 
-pipes = "|-LJ7F"
-ans = -1
-for i in range(1000000):
-    v.add(cp)
-    n = []
-    match grid[cp[0]][cp[1]]:
-        case "|":
-            n.append((cp[0] + 1, cp[1]))
-            n.append((cp[0] - 1, cp[1]))
-        case "-":
-            n.append((cp[0], cp[1] - 1))
-            n.append((cp[0], cp[1] + 1))
-        case "L":
-            n.append((cp[0] - 1, cp[1]))
-            n.append((cp[0], cp[1] + 1))
-        case "J":
-            n.append((cp[0] - 1, cp[1]))
-            n.append((cp[0], cp[1] - 1))
-        case "7":
-            n.append((cp[0] + 1, cp[1]))
-            n.append((cp[0], cp[1] - 1))
-        case "F":
-            n.append((cp[0] + 1, cp[1]))
-            n.append((cp[0], cp[1] + 1))
-    nx = [x for x in n if x not in v]
-    if len(nx) == 0:
-        ans = i
-        break
-    cp = nx[0]
-# print(ans // 2 + 1)
-
-# ans = 0
-# tv = set()
-# for i in range(1,len(grid) - 1):
-
-ap = set()
-for i, line in enumerate(lines):
-    for j, x in enumerate(line):
-        if x == '.':
-            ap.add((i, j))
-for pp in ap.copy():
-    if pp[0] == 0 or pp[0] == len(grid) - 1:
-        ap.remove(pp)
-        continue
-
-    if pp[1] == 0 or pp[1] == len(grid[0]) - 1:
-        ap.remove(pp)
-
-
-def filter_ap_by_neighbor(points):
-    # up
-    for p in points.copy():
-        det = (p[0] - 1, p[1])
-        if grid[det[0]][det[1]] == '.' and det not in points:
-            points.remove(p)
-            continue
-        # down
-        det = (p[0] + 1, p[1])
-        if grid[det[0]][det[1]] == '.' and det not in points:
-            points.remove(p)
-            continue
-
-        # left
-        det = (p[0], p[1] - 1)
-        if grid[det[0]][det[1]] == '.' and det not in points:
-            points.remove(p)
-            continue
-
-        # right
-        det = (p[0], p[1] + 1)
-        if grid[det[0]][det[1]] == '.' and det not in points:
-            points.remove(p)
-            continue
-
-
-while True:
-    pre_len = len(ap)
-    filter_ap_by_neighbor(ap)
-    if len(ap) == pre_len:
-        break
-
-for i, line in enumerate(grid):
-    tp = []
-    for j, x in enumerate(line):
-        if x == '.':
-            if (i, j) in ap:
-                tp.append('T')
-            else:
-                tp.append('O')
-        else:
-            tp.append(x)
-    print(''.join(tp))
-
-print(len(ap))
+ans = 0
+for i in range(len(lines)):
+    sin = False
+    for j in range(len(lines[0])):
+        if (i, j) in pipes and grid[(i, j)] in '|7F':
+            sin = not sin
+        ans += (i, j) not in pipes and sin
+print(ans)
---
+++ 2023/day11.py
@@ -0,0 +1,51 @@
+import itertools
+
+from common import io_utils
+
+data = """
+...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....
+"""
+
+data = io_utils.get_data(2023, 11)
+lines = data.strip().splitlines()
+wx = [1] * len(lines[0])
+wy = [1] * len(lines[0])
+print(len(lines), len(lines[0]))
+for i, line in enumerate(lines):
+    if sum(1 for x in line if x == '.') == len(line):
+        wx[i] = 1000000
+        # wx[i] = 100
+        # wx[i] = 10
+
+for i in range(len(lines[0])):
+    if sum(lines[j][i] == '.' for j in range(len(lines))) == len(lines):
+        for j in range(len(lines)):
+            wy[i] = 1000000
+            # wy[i] = 100
+            # wy[i] = 10
+
+print(wx)
+print(wy)
+
+grid = {(i, j): lines[i][j] for i, j in itertools.product(range(len(lines)), range(len(lines[0])))}
+
+ss = [(i, j) for i, j in grid.keys() if grid[i, j] == '#']
+
+ans = 0
+for p in ss:
+    r = ss.copy()
+    r.remove(p)
+    for p2 in r:
+        ans += sum(wx[i] for i in range(min(p[0], p2[0]), max(p[0], p2[0])))
+        ans += sum(wy[i] for i in range(min(p[1], p2[1]), max(p[1], p2[1])))
+
+print(ans // 2)
