09611c0cf92ccca9b81d62ae3068c8f250002492
guohaoice <guohaoice@gmail.com>
2023-12-12 08:44:50 +0800

    2022 day11 p2

---
+++ 2022/day11p2.py
@@ -0,0 +1,44 @@
+import re
+
+from common import io_utils
+
+data = io_utils.get_data(2022, 11)
+parts = data.strip().split('\n\n')
+MS = []
+opd = {'+': lambda a, b: a + b,
+       '-': lambda a, b: a - b,
+       '*': lambda a, b: a * b,
+       '/': lambda a, b: a // b,
+       '**': lambda a, b: pow(a, 2)
+       }
+
+N = 1
+for p in parts:
+    p = p.splitlines()[1:]
+    m = [[], None, 0, 0, 0, 0, 0]
+    m[0] = [int(x) for x in re.findall(r'\d+', p[0])]
+    if 2 == len(re.findall('old', p[1])):
+        m[1] = opd['**']
+    else:
+        m[1] = opd[re.findall(r'[+\-*/]', p[1])[0]]
+        m[2] = int(re.findall(r'\d+', p[1])[0])
+    m[3] = int(re.findall(r'\d+', p[2])[0])
+    N *= m[3]
+    m[4] = int(re.findall(r'\d+', p[3])[0])
+    m[5] = int(re.findall(r'\d+', p[4])[0])
+    m[6] = 0
+    MS.append(m)
+for i in range(10000):
+    for m in MS:
+        m[6] += len(m[0])
+        c = [m[1](x, m[2]) % N for x in m[0]]
+        for x in c:
+            if x % m[3] == 0:
+                MS[m[4]][0].append(x)
+            else:
+                MS[m[5]][0].append(x)
+        m[0].clear()
+
+MS.sort(key=lambda x: x[6])
+
+print(MS[-1][6] * MS[-2][6])
