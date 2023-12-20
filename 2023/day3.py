7c4f3508a608c7374ed406e8acc8e621da02e335
guohaoice <guohaoice@gmail.com>
2023-12-20 18:32:36 +0800

    refactor day3

--- 2023/day3.py
+++ 2023/day3.py
@@ -6,20 +6,6 @@ from helper import *
 # 525181
 # 84289137
 data = raw_data(2023, 3)
-
-data = """
-467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..
-"""
-data = data.strip()
 lines = lines(data)
 
 ans = 0
@@ -33,27 +19,15 @@ for i, line in enumerate(lines):
                 break
 print(ans)
 
-g = grid_2d(lines)
-ans = 0
-not_dot = set(x[1] for x in g.items() if not x[1].isdigit() and x[1] != '.')
-print(not_dot)
-
-points = [item for c in not_dot for item in g.findall(c)]
-for p in points:
-    move = p.moves(diagonals=True)
-    ns = [n for n in p.neighbors(move) if g[n].isdigit()]
-    if len(ns) ==2:
-# print(points)
-for line in lines:
-    d = defaultdict(lambda: [])
-    for i, line in enumerate(lines):
-        for m in regex.finditer(r'\d+', line):
-            for x, y in itertools.product(range(i - 1, i + 2), range(m.start() - 1, m.end() + 1)):
-                if not (0 <= x < len(lines[0]) and 0 <= y < len(lines)):
-                    continue
-                if lines[x][y] != '.' and not lines[x][y].isdigit():
-                    d[(x, y)].append(int(m.group()))
-                    break
-    ans += sum(v[0] * v[1] for v in d.values() if len(v) == 2)
+d = defaultdict(lambda: [])
+for i, line in enumerate(lines):
+    for m in regex.finditer(r'\d+', line):
+        for x, y in itertools.product(range(i - 1, i + 2), range(m.start() - 1, m.end() + 1)):
+            if not (0 <= x < len(lines[0]) and 0 <= y < len(lines)):
+                continue
+            if lines[x][y] != '.' and not lines[x][y].isdigit():
+                d[(x, y)].append(int(m.group()))
+                break
+ans = sum(v[0] * v[1] for v in d.values() if len(v) == 2)
 
 print(ans)
