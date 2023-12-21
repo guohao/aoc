04d127b5dd27daa3a5d9567d6fc0eb74926175ff
guohaoice <guohaoice@gmail.com>
2023-12-21 20:22:33 +0800

    day21

--- 2023/day21.py
+++ 2023/day21.py
@@ -1,27 +1,48 @@
+from collections import deque
+
 import nographs as nog
 from helper import *
 
 data = raw_data(2023, 21)
-g = nog.Array(data.strip().splitlines(), 2)
+lines = lines(data.strip())
+g = nog.Array(lines, 2)
+H = len(lines)
 
 
-def dfs(start, depth):
-    def next_vertices(points, _):
-        moves = nog.Position.moves(2)
+def bfs(start, steps):
+    dq = deque()
+    dq.append({start})
+    const_num = [0, 0]
+    mem = deque(maxlen=2)
+    mem.append(set())
+    mem.append(set())
+    ax = [0, 0, 0]
+    for i in range(1, steps + 1):
+        points = dq.popleft()
         next_visit = set()
+        visited = mem.popleft()
         for point in points:
-            for neighbor in point.neighbors(moves, g.limits()):
-                if g[neighbor] != "#":
+            for neighbor in point.neighbors(nog.Position.moves(2)):
+                if neighbor in visited:
+                    continue
+                wrap = neighbor.wrap_to_cuboid(g.limits())
+                if g[wrap] != "#":
                     next_visit.add(neighbor)
-        if next_visit:
-            yield tuple(next_visit)
+        const_num[i % 2] += len(visited)
+        mem.append(next_visit)
+        dq.append(next_visit)
+        if i % H == steps % H:
+            ax[i // H] = len(next_visit) + const_num[i % 2]
+            if ax[2] != 0:
+                break
+    n = steps // H
+    a = (ax[2] + ax[0] - 2 * ax[1]) / 2
+    b = ax[1] - ax[0] - a
+    c = ax[0]
 
-    traversal = nog.TraversalBreadthFirst(next_vertices)
-    found = next(traversal.start_from(start_vertices=((start,),),
-                                      build_paths=True
-                                      ).go_for_depth_range(depth, depth + 1))
-    return len(traversal.paths[found][-1])
+    return int(a * n ** 2 + b * n + c)
 
 
 S = g.findall('S')[0]
-print(dfs(S, 64))
+print(bfs(S, 64))
+print(bfs(S, 26501365))
