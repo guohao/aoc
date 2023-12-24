c1660cb4c029860023f7754f08fe8288a52dbf2a
guohaoice <guohaoice@gmail.com>
2023-12-24 16:26:07 +0800

    Stash

--- 2022/day17p2.py
+++ 2022/day17p2.py
@@ -2,17 +2,17 @@ from itertools import cycle
 
 from helper import *
 
-MOVES = {'<': 1, '>': 0}
+MOVES = {'<': -1, '>': 1}
 
 data = """
 >>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>
 """
 
-data = raw_data(2022, 17)
+# data = raw_data(2022, 17)
 JETS = cycle(list(data.strip()))
 ROCKS = cycle([
     [1, 1, 1, 1],
-    [3, 7, 3],
+    [2, 7, 2],
     [1, 1, 7],
     [15],
     [3, 3]
@@ -23,14 +23,14 @@ G = [0] * N
 
 
 def print_bin(x: List[int]):
-    for i in x:
-        print(bin(i)[2:].zfill(7).replace('1', '#').replace('0', '+'))
+    for n in range(max(x).bit_length() - 1, -1, -1):
+        print(''.join(['#' if (i >> n) & 1 else '.' for i in x]))
 
 
 def can_move(rock, left, down):
     if down < 0:
         return False
-    if left < 0 or left + len(rock) >= N:
+    if left < 0 or left + len(rock) > N:
         return False
     for j in range(len(rock)):
         if G[left + j] & (rock[j] << down):
@@ -43,9 +43,30 @@ def do_move(rock, left, down):
         G[left + j] |= rock[j] << down
 
 
+MIN_CYCLE_LEN = lcm([5, len(data)])
+
 def p(epoch):
-    for _ in range(0, epoch):
-        down = max(G).bit_length() + 3
+    height = 0
+    prev_h = -1
+    diff = []
+    for i in range(0, epoch):
+        down = height + 3
+        if i > MIN_CYCLE_LEN and epoch % MIN_CYCLE_LEN == i % MIN_CYCLE_LEN:
+            if prev_h>-1:
+                diff.append(height-prev_h)
+            prev_h = height
+            if diff :
+                print(diff)
+                # lt, rt = seq[:len(seq) // 2], seq[len(seq) // 2:]
+                # print(lt, rt)
+                # if lt == rt:
+                #     print('period', lt, 'len:', len(lt))
+                #     remain = epoch - i
+                #     cycle_len = len(lt)
+                #     part_cycle_sum = sum(lt[:remain % cycle_len // 5])
+                #     total = remain // cycle_len * sum(lt) + part_cycle_sum + prev
+                #     print(total)
+                #     break
         rock = next(ROCKS)
         left = 2
         while True:
@@ -57,8 +78,11 @@ def p(epoch):
             else:
                 do_move(rock, left, down)
                 break
+        height = max(height, down + max(rock).bit_length())
+    print(height)
 
 
-p(1)
-print_bin(G)
-print(max(G).bit_length())
+p(2022)
+# print_bin(G)
+# print(max(G).bit_length())
+# p(1000000000000)
