325e0261ba5875877f21fb2dd18df8d3d3bd8b73
guohaoice <guohaoice@gmail.com>
2023-12-25 18:49:58 +0800

    day25

--- 2023/day25.py
+++ 2023/day25.py
@@ -1,7 +1,17 @@
+import networkx as nx
 from helper import *
 
-data= """
-
-"""
-# data = raw_data(2023,)
+data = raw_data(2023, 25)
 lines = lines(data)
+graph = nx.DiGraph()  # Create an instance of DiGraph
+for line in lines:
+    left, right = line.split(':')
+    for y in right.split():
+        graph.add_edge(left, y, capacity=1)
+        graph.add_edge(y, left, capacity=1)
+
+for a, b in itertools.combinations(graph.nodes, 2):
+    cut_c, (left, right) = nx.minimum_cut(graph, a, b)
+    if cut_c == 3:
+        print(len(left) * len(right))
+        break
