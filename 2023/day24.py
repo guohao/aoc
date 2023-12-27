dde35f24b48822583601c565767b6bcd6a00fa83
guohao <guohaoice@gmail.com>
2023-12-27 15:38:07 +0800

    day24

--- 2023/day24.py
+++ 2023/day24.py
@@ -1,15 +1,12 @@
 from helper import *
+from z3 import *
 
 data = raw_data(2023, 24)
 lines = lines(data)
 HS = []
 for line in lines:
-    x, y, _, dx, dy, _ = map(int, re.findall('[+-]?\d+', line))
-    # xt = x0 + t*dx
-    # yt = y0 + t*dy
-    # x0+ t*dx0 = x1 + t*dx1
-    # y0 + t*dy0 = y1 + t*dy1
-    HS.append((x, y, dx, dy))
+    x, y, z, dx, dy, dz = map(int, re.findall(r'[+-]?\d+', line))
+    HS.append((x, y, z, dx, dy, dz))
 
 ans = 0
 MIN = 200000000000000
@@ -18,20 +15,8 @@ for i in range(len(HS)):
     for j in range(i):
         a = HS[i]
         b = HS[j]
-        ax, ay, adx, ady = a
-        bx, by, bdx, bdy = b
-        # x0 = adx * ta + ax
-        # y0 = ady * ta + ay
-        # x1 = bdx * tb + bx
-        # y1 = bdy * tb + by
-        # x0 = x1
-        # adx * ta + ax =  bdx * tb + bx
-        # ady * ta + ay  = bdy * tb + by
-        # ta =(bdx * tb + bx -ax )/ adx
-        # ady * (bdx * tb + bx -ax )/ adx + ay = bdy * tb + by
-        # ady * bdx * tb + ady*(bx-ax) = adx*bdy *tb + adx*by - adx*ay
-        # (ady * bdx -adx*bdy )*tb = adx*(by-ay) - ady*(bx-ax))
-        # tb = (adx*by + ady*ax - ady*bx) / (ady * bdx - adx * bdy)
+        ax, ay, _, adx, ady, _ = a
+        bx, by, _, bdx, bdy, _ = b
         if ady * bdx == adx * bdy:
             continue
         tb = (adx * (by - ay) - ady * (bx - ax)) / (ady * bdx - adx * bdy)
@@ -46,3 +31,14 @@ for i in range(len(HS)):
             ans += 1
 
 print(ans)
+
+x, y, z, dx, dy, dz = Int('x'), Int('y'), Int('z'), Int('dx'), Int('dy'), Int('dz')
+
+T = [Int(f'T{i}') for i in range(len(HS))]
+S = Solver()
+for i in range(len(HS)):
+    S.add(x + T[i] * dx - HS[i][0] - T[i] * HS[i][3] == 0)
+    S.add(y + T[i] * dy - HS[i][1] - T[i] * HS[i][4] == 0)
+    S.add(z + T[i] * dz - HS[i][2] - T[i] * HS[i][5] == 0)
+S.check()
+print(S.model().eval(x + y + z))
