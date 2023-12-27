31833f35f1351380a63cb2f9a433e38b3af3e9fa
guohao <guohaoice@gmail.com>
2023-12-27 18:01:28 +0800

    day13

--- 2023/day13.py
+++ 2023/day13.py
@@ -1,27 +1,36 @@
-from helper import *
+import helper
 
-data = raw_data(2023, 13)
-
-
-def f(lines: List[str], diff: int) -> int:
-    n = len(lines)
-    c = 0
-    for i in range(1, n):
-        r = min(n - i, i)
-        a = lines[i - r:i + r]
-        b = a[::-1]
-        if count_differences_in_lists(a, b) == diff:
-            c += i
-    return c
-
-
-ans = 0
+ans1 = 0
 ans2 = 0
-for p in patterns(data):
-    ans += 100 * f(p, 0)
-    ans += f(rotate_matrix_90_clockwise(p), 0)
-    ans2 += 100 * f(p, 2)
-    ans2 += f(rotate_matrix_90_clockwise(p), 2)
+for part in helper.raw_data(2023, 13).strip().split('\n\n'):
+    lines = helper.lines(part)
+    for i in range(1, len(lines)):
+        if i * 2 < len(lines):
+            size = i
+        else:
+            size = len(lines) - i
+        up = lines[i - size:i]
+        down = lines[i:i + size][::-1]
+        # if up == down:
+        diff_count = 0
+        for j in range(len(lines[0])):
+            diff_count += sum(1 for k in range(size) if up[k][j] != down[k][j])
+        if diff_count == 0:
+            ans1 += 100 * i
+        if diff_count == 1:
+            ans2 += 100 * i
 
-print(ans)
+    for j in range(1, len(lines[0])):
+        size = min(j, len(lines[0]) - j)
+        diff_count = 0
+        for i in range(len(lines)):
+            left = lines[i][j - size:j]
+            right = lines[i][j:j + size][::-1]
+            diff_count += sum(1 for k in range(size) if left[k] != right[k])
+        if diff_count == 0:
+            ans1 += j
+        if diff_count == 1:
+            ans2 += j
+
+print(ans1)
 print(ans2)
