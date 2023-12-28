c3965b5c3cb14c5769ca39bebcf22061a9e7b246
guohao <guohaoice@gmail.com>
2023-12-28 11:43:43 +0800

    day2 again

--- 2023/day2.py
+++ 2023/day2.py
@@ -4,20 +4,44 @@ from functools import reduce
 from helper import *
 
 
-def f(i, line):
-    if not re.findall(r'((1[3-9]|[2-9]\d) red)|((1[5-9]|[2-9]\d) blue)|((1[4-9]|[2-9]\d) green)', line):
-        return i + 1
-    return 0
+def extract_count(line: str):
+    counts = []
+    for reveal in line.split(':')[1].split(";"):
+        for play in reveal.split(','):
+            count, color = play.split()
+            counts.append((int(count), color.strip()))
+    return counts
 
 
-def f2(line):
-    d = defaultdict(lambda: 0)
-    for m in re.finditer(r'\d+\s+(blue|green|red)', line):
-        count, color = m.group().split(' ')
-        d[color] = max(int(count), d[color])
-    return reduce(lambda a, b: a * b, d.values())
+def p1():
+    ans = 0
+    for i, line in enumerate(lines, start=1):
+        match = True
+        for count, color in extract_count(line):
+            if color == 'red' and count > 12:
+                match = False
+                break
+            elif color == 'green' and count > 13:
+                match = False
+                break
+            elif color == 'blue' and count > 14:
+                match = False
+                break
+        if match:
+            ans += i
+    print(ans)
+
+
+def p2():
+    ans = 0
+    for i, line in enumerate(lines, start=1):
+        fewest = defaultdict(lambda: 0)
+        for count, color in extract_count(line):
+            fewest[color] = max(count, fewest[color])
+        ans += reduce(lambda a, b: a * b, fewest.values())
+    print(ans)
 
 
 lines = lines(raw_data(2023, 2))
-print(sum(f(i, line) for i, line in enumerate(lines)))
-print(sum(f2(line) for line in lines))
+p1()
+p2()
