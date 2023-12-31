a315f9af9e00db00689cbc66e0f864049b7060df
guohao <guohaoice@gmail.com>
2023-12-31 23:06:48 +0800

    2022 day18

--- 2022/day18.py
+++ 2022/day18.py
@@ -1,4 +1,6 @@
 import functools
+import sys
+from collections import deque
 
 from helper import *
 
@@ -18,7 +20,7 @@ data = """
 2,3,5
 """
 
-# data = raw_data(2022, 18)
+data = raw_data(2022, 18)
 lines = lines(data)
 
 cubes = [nums(line) for line in lines]
@@ -27,6 +29,26 @@ for i, a in enumerate(cubes):
     for b in cubes[:i]:
         if sum(abs(a[k] - b[k]) for k in range(3)) == 1:
             ans -= 2
-    for c in itertools.product([0, 1, -1], repeat=3):
-        print(c)
 print(ans)
+
+lower_bound = [min(c[i] - 1 for c in cubes) for i in range(3)]
+upper_bound = [max(c[i] + 1 for c in cubes) for i in range(3)]
+
+ans2 = 0
+dq = deque()
+dq.append(lower_bound)
+seen = set()
+while dq:
+    v = dq.popleft()
+    if v in cubes:
+        ans2 += 1
+        continue
+    if tuple(v) not in seen:
+        seen.add(tuple(v))
+        for i, j in itertools.product(range(0, 3), (-1, 1)):
+            nv = v.copy()
+            nv[i] += j
+            if all(lower_bound[i] <= nv[i] <= upper_bound[i] for i in range(3)):
+                dq.append(nv)
+
+print(ans2)
