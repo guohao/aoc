1979d2bc93d7c349e1047753fd0490ef2fa2ae93
guohao <guohaoice@gmail.com>
2024-01-04 19:15:04 +0800

    day22 p2

--- 2022/day22.py
+++ 2022/day22.py
@@ -1,40 +1,12 @@
-import re
-
 from helper import *
 
-data = """
-        ...#
-        .#..
-        #...
-        ....
-...#.......#
-........#...
-..#....#....
-..........#.
-        ...#....
-        .....#..
-        .#......
-        ......#.
-
-10R5L5R10L4R5L5
-"""
 data = raw_data(2022, 22)
 maze, path = data.split('\n\n')
 if maze[0] == '\n':
     maze = maze[1:]
 path = 'R' + path
 
-g = {}
-
-
-def parse_input():
-    for i, line in enumerate(maze.splitlines()):
-        for j, c in enumerate(line):
-            if c != ' ':
-                g[(i, j)] = c
-
-
-parse_input()
+g = {(i, j): c for i, line in enumerate(maze.splitlines()) for j, c in enumerate(line) if c != ' '}
 limits = (min(x for x, _ in g), max(x for x, _ in g), min(y for _, y in g), max(y for _, y in g))
 N = limits[1] - limits[0] + 1
 M = limits[3] - limits[2] + 1
@@ -57,8 +29,6 @@ def solve():
     p = (0, maze.index('.'))
     facing = (-1, 0)
     for move in re.findall(r'[RL]\d+', path):
-
-        print(move)
         steps = int(move[1:])
         direction = move[0]
         if direction == 'R':
@@ -66,14 +36,6 @@ def solve():
         else:
             facing = (-facing[1], facing[0])
         for i in range(steps):
-            # if facing == (0, 1):
-            #     g[p] = '>'
-            # elif facing == (1, 0):
-            #     g[p] = 'v'
-            # elif facing == (0, -1):
-            #     g[p] = '<'
-            # else:
-            #     g[p] = '^'
             np = neighbor(p, facing)
             if np:
                 p = np
@@ -90,19 +52,8 @@ def solve():
             ans += 2
         case (-1, 0):
             ans += 3
-    # print(facing)
-    print(p)
     print(ans)
     return
 
 
 solve()
-
-# for i in range(N):
-#     line = ''
-#     for j in range(M):
-#         if (i, j) in g:
-#             line += g[i, j]
-#         else:
-#             line += ' '
-#     print(line)
--- 2022/day22p2.py
+++ 2022/day22p2.py
@@ -1,56 +1,23 @@
-import re
-
 from helper import *
 
-data = """
-        ...#
-        .#..
-        #...
-        ....
-...#.......#
-........#...
-..#....#....
-..........#.
-        ...#....
-        .....#..
-        .#......
-        ......#.
-
-10R5L5R10L4R5L5
-"""
 data = raw_data(2022, 22)
 maze, path = data.split('\n\n')
 if maze[0] == '\n':
     maze = maze[1:]
 path = 'R' + path
 
-g = {}
+g = {(i, j): c for i, line in enumerate(maze.splitlines()) for j, c in enumerate(line) if c != ' '}
+
+neighbors = {}
 
 
-def parse_input():
-    for i, line in enumerate(maze.splitlines()):
-        for j, c in enumerate(line):
-            if c != ' ':
-                g[(i, j)] = c
-
-
-parse_input()
-limits = (min(x for x, _ in g), max(x for x, _ in g), min(y for _, y in g), max(y for _, y in g))
-N = limits[1] - limits[0] + 1
-M = limits[3] - limits[2] + 1
-
-
-def neighbor(p, facing) -> (int, int):
-    assert p
-    limit = N if facing[0] != 0 else M
-    for _ in range(limit):
-        np = ((p[0] + facing[0]) % N, (p[1] + facing[1]) % M)
-        if np in g:
-            if g[np] == '.':
-                return np
-            elif g[np] == '#':
-                return None
-        p = np
+def neighbor(p, facing) -> ((int, int), (int, int)):
+    np = ((p[0] + facing[0]), (p[1] + facing[1]))
+    if np not in g:
+        np, facing = neighbors[p, facing]
+    if g[np] != '#':
+        return np, facing
+    return None, None
 
 
 def solve():
@@ -63,40 +30,37 @@ def solve():
     p = (0, maze.index('.'))
     facing = (-1, 0)
     K = 50
-    neighbors = {}
     for i in range(0, K):
         # A -> F
         neighbors[(0, K + i), (-1, 0)] = ((3 * K + i, 0), (0, 1))
         # A -> E
-        neighbors[(K, i), (-1, 0)] = ((3 * K - i, 0), (0, 1))
+        neighbors[(i, K), (0, -1)] = ((3 * K - 1 - i, 0), (0, 1))
         # B -> C
-        neighbors[(K, 2 * K + i), (1, 0)] = ((K + i, 2 * K - 1), (0, -1))
+        neighbors[(K - 1, 2 * K + i), (1, 0)] = ((K + i, 2 * K - 1), (0, -1))
         # B -> F
         neighbors[(0, 2 * K + i), (-1, 0)] = (4 * K - 1, i), (-1, 0)
-        # B->D
-        neighbors[(3 * K - 1, i), (0, 1)] = ((3 * K - i, 2 * K - 1), (0, -1))
-        # C ->E
+        # B -> D
+        neighbors[(i, 3 * K - 1), (0, 1)] = ((3 * K - 1 - i, 2 * K - 1), (0, -1))
+        # C -> E
         neighbors[(K + i, K), (0, -1)] = ((2 * K, i), (1, 0))
-        # C->B
+        # C -> B
         neighbors[(K + i, 2 * K - 1), (0, 1)] = ((K - 1, 2 * K + i), (-1, 0))
-        # D->B
+        # D -> B
         neighbors[(2 * K + i, 2 * K - 1), (0, 1)] = ((K - 1 - i, 3 * K - 1), (0, -1))
-        # D->F
+        # D -> F
         neighbors[(3 * K - 1, K + i), (1, 0)] = ((3 * K + i, K - 1), (0, -1))
-        # E ->A
-        neighbors[(3 * K + i, 0), (0, -1)] = ((0, K + i), (-1, 0))
-        # E->C
+        # E -> A
+        neighbors[(2 * K + i, 0), (0, -1)] = ((K - 1 - i, K), (0, 1))
+        # E -> C
         neighbors[(2 * K, i), (-1, 0)] = ((K + i, K), (0, 1))
         # F -> A
-        neighbors[(3 * K - i, 0), (0, -1)] = ((3 * K - i, 0), (1, 0))
+        neighbors[(3 * K + i, 0), (0, -1)] = ((0, K + i), (1, 0))
         # F -> B
         neighbors[(4 * K - 1, i), (1, 0)] = ((0, 2 * K + i), (1, 0))
         # F -> D
         neighbors[(3 * K + i, K - 1), (0, 1)] = ((3 * K - 1, K + i), (-1, 0))
 
     for move in re.findall(r'[RL]\d+', path):
-
-        print(move)
         steps = int(move[1:])
         direction = move[0]
         if direction == 'R':
@@ -104,19 +68,10 @@ def solve():
         else:
             facing = (-facing[1], facing[0])
         for i in range(steps):
-            # if facing == (0, 1):
-            #     g[p] = '>'
-            # elif facing == (1, 0):
-            #     g[p] = 'v'
-            # elif facing == (0, -1):
-            #     g[p] = '<'
-            # else:
-            #     g[p] = '^'
-            np = neighbor(p, facing)
+            np, nf = neighbor(p, facing)
             if np:
                 p = np
-            else:
-                break
+                facing = nf
 
     ans = 1000 * (p[0] + 1) + 4 * (p[1] + 1)
     match facing:
@@ -128,19 +83,8 @@ def solve():
             ans += 2
         case (-1, 0):
             ans += 3
-    # print(facing)
-    print(p)
     print(ans)
     return
 
 
 solve()
-
-# for i in range(N):
-#     line = ''
-#     for j in range(M):
-#         if (i, j) in g:
-#             line += g[i, j]
-#         else:
-#             line += ' '
-#     print(line)
