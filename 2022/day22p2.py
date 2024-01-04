491feadeecf16e6a5261d3faf151ae0eaf7efb37
guohaoice <guohaoice@gmail.com>
2024-01-04 09:51:41 +0800

    day22 p2 unfinsh

--- 2022/day22p2.py
+++ 2022/day22p2.py
@@ -54,8 +54,46 @@ def neighbor(p, facing) -> (int, int):
 
 
 def solve():
+    """
+      .AB
+      .C.
+      ED.
+      F..
+      """
     p = (0, maze.index('.'))
     facing = (-1, 0)
+    K = 50
+    neighbors = {}
+    for i in range(0, K):
+        # A -> F
+        neighbors[(0, K + i), (-1, 0)] = ((3 * K + i, 0), (0, 1))
+        # A -> E
+        neighbors[(K, i), (-1, 0)] = ((3 * K - i, 0), (0, 1))
+        # B -> C
+        neighbors[(K, 2 * K + i), (1, 0)] = ((K + i, 2 * K - 1), (0, -1))
+        # B -> F
+        neighbors[(0, 2 * K + i), (-1, 0)] = (4 * K - 1, i), (-1, 0)
+        # B->D
+        neighbors[(3 * K - 1, i), (0, 1)] = ((3 * K - i, 2 * K - 1), (0, -1))
+        # C ->E
+        neighbors[(K + i, K), (0, -1)] = ((2 * K, i), (1, 0))
+        # C->B
+        neighbors[(K + i, 2 * K - 1), (0, 1)] = ((K - 1, 2 * K + i), (-1, 0))
+        # D->B
+        neighbors[(2 * K + i, 2 * K - 1), (0, 1)] = ((K - 1 - i, 3 * K - 1), (0, -1))
+        # D->F
+        neighbors[(3 * K - 1, K + i), (1, 0)] = ((3 * K + i, K - 1), (0, -1))
+        # E ->A
+        neighbors[(3 * K + i, 0), (0, -1)] = ((0, K + i), (-1, 0))
+        # E->C
+        neighbors[(2 * K, i), (-1, 0)] = ((K + i, K), (0, 1))
+        # F -> A
+        neighbors[(3 * K - i, 0), (0, -1)] = ((3 * K - i, 0), (1, 0))
+        # F -> B
+        neighbors[(4 * K - 1, i), (1, 0)] = ((0, 2 * K + i), (1, 0))
+        # F -> D
+        neighbors[(3 * K + i, K - 1), (0, 1)] = ((3 * K - 1, K + i), (-1, 0))
+
     for move in re.findall(r'[RL]\d+', path):
 
         print(move)
