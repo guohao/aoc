41d2bc17e52046f05fe29f58071a59d967ca7a92
guohao <guohaoice@gmail.com>
2024-03-04 11:14:11 +0800

    day15

---
+++ 2017/day15.py
@@ -0,0 +1,40 @@
+from helper import *
+
+
+def p1():
+    a = 591
+    b = 393
+    fa = 16807
+    fb = 48271
+    ans = 0
+    for _ in range(40000000):
+        a = (a * fa) % 2147483647
+        b = (b * fb) % 2147483647
+        if a & 0xffff == b & 0xffff:
+            ans += 1
+    print(ans)
+
+
+def gen(a, fa, m):
+    while True:
+        a = (a * fa) % 2147483647
+        if a % m == 0:
+            yield a & 0xffff
+
+
+def p2():
+    a = 591
+    b = 393
+    fa = 16807
+    fb = 48271
+    ans = 0
+    ga = iter(gen(a, fa, 4))
+    gb = iter(gen(b, fb, 8))
+    for _ in range(5000000):
+        if next(ga) == next(gb):
+            ans += 1
+    print(ans)
+
+
+p1()
+p2()
