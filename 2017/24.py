1c3919d11fdfd94f87ecc3200f623da10c7ff77f
guohao <guohaoice@gmail.com>
2024-03-07 21:03:06 +0800

    day24

---
+++ 2017/24.py
@@ -0,0 +1,50 @@
+import networkx as nx
+
+
+def p1(data: str):
+    cmps = []
+    G = nx.Graph()
+    for line in data.splitlines():
+        a, b = map(int, line.split('/'))
+        cmps.append((a, b))
+        G.add_node((a, b))
+    cmps = set(cmps)
+
+    def dfs(visited: tuple, pn=None):
+        ans = 0
+        for c in cmps - set(visited):
+            if pn not in c:
+                continue
+            npn = c[0] if c[1] == pn else c[1]
+            ans = max(ans, sum(c) + dfs(visited + (c,), npn))
+        return ans
+
+    return dfs((), 0)
+
+
+def p2(data: str):
+    cmps = []
+    G = nx.Graph()
+    for line in data.splitlines():
+        a, b = map(int, line.split('/'))
+        cmps.append((a, b))
+        G.add_node((a, b))
+    cmps = set(cmps)
+
+    def dfs(visited: tuple, pn=None):
+        length, strength = 0, 0
+        for c in cmps - set(visited):
+            if pn not in c:
+                continue
+            npn = c[0] if c[1] == pn else c[1]
+            nl, ns = dfs(visited + (c,), npn)
+            nl += 1
+            ns += sum(c)
+            if nl > length:
+                length = nl
+                strength = ns
+            elif nl == length:
+                strength = max(ns, strength)
+        return length, strength
+
+    return dfs((), 0)[1]
