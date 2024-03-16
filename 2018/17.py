2e11c3b5ad422c5090d49ba63f4b40edf6260f09
guohaoice <guohaoice@gmail.com>
2024-03-17 00:11:10 +0800

    dfs

--- 2018/17.py
+++ 2018/17.py
@@ -1,77 +1,54 @@
 import re
 from collections import deque
 from functools import cache
+import sys
 
-
-def build_g(data: str):
-    G = {}
-    for line in data.splitlines():
-        d = {}
-        for cell in line.split(', '):
-            l, r = cell.split('=')
-            rg = list(map(int, re.findall(r'\d+', r)))
-            d[l] = rg
-        for x in range(d['x'][0], d['x'][-1] + 1):
-            for y in range(d['y'][0], d['y'][-1] + 1):
-                G[x, y] = '#'
-    return G
+sys.setrecursionlimit(10000)
 
 
 def solve(data: str):
-    G = build_g(data)
+    G = {}
+
+    def build_g():
+        for line in data.splitlines():
+            d = {}
+            for cell in line.split(', '):
+                l, r = cell.split('=')
+                rg = list(map(int, re.findall(r'\d+', r)))
+                d[l] = rg
+            for x in range(d['x'][0], d['x'][-1] + 1):
+                for y in range(d['y'][0], d['y'][-1] + 1):
+                    G[x, y] = '#'
+
+    build_g()
     min_y = min(y for _, y in G)
     max_y = max(y for _, y in G)
-    min_x = min(x for x, _ in G)
-    max_x = max(x for x, _ in G)
 
     def is_solid(x, y):
         return (x, y) in G and G[x, y] in '~#'
 
-    def drop():
-        q = deque()
+    def drop(x, y):
+        if (x, y) in G:
+            return
+        G[x, y] = '|'
+        if y > max_y:
+            return
+        drop(x, y + 1)
+        if not is_solid(x, y + 1):
+            return
+        drop(x - 1, y)
+        drop(x + 1, y)
+        xl = x - 1
+        while (xl, y) in G and G[xl, y] == '|':
+            xl -= 1
+        xr = x + 1
+        while (xr, y) in G and G[xr, y] == '|':
+            xr += 1
+        if is_solid(xl, y) and is_solid(xr, y):
+            for i in range(xl + 1, xr):
+                G[i, y] = '~'
 
-        def drop_down(x, y) -> bool:
-            if y + 1 > max_y:
-                return False
-            if (x, y + 1) not in G:
-                q.append((x, y))
-                q.append((x, y + 1))
-                return False
-            if not is_solid(x, y + 1):
-                return False
-            return True
-
-        def drop_left(x, y, op):
-            if (x + op, y) not in G:
-                q.append((x, y))
-                q.append((x + op, y))
-
-        def drain_water(x, y):
-            xl = x - 1
-            while (xl, y) in G and G[xl, y] == '|':
-                xl -= 1
-            xr = x + 1
-            while (xr, y) in G and G[xr, y] == '|':
-                xr += 1
-            if is_solid(xl, y) and is_solid(xr, y):
-                for i in range(xl + 1, xr):
-                    G[i, y] = '~'
-
-        q.append((500, 0))
-        while q:
-            k = q.pop()
-            x, y = k
-            if is_solid(x, y):
-                continue
-            if k not in G:
-                G[x, y] = '|'
-            if not drop_down(x, y):
-                continue
-            drop_left(x, y, -1)
-            drop_left(x, y, 1)
-            drain_water(x, y)
-
-    drop()
+    drop(500, 0)
     return [v for (_, y), v in G.items() if min_y <= y <= max_y]
 
 
