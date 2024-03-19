ae14290357a5727d0aad9ad385fe560525eb4bd2
guohao <guohaoice@gmail.com>
2024-03-19 11:32:40 +0800

    22

--- 2018/22.py
+++ 2018/22.py
@@ -9,16 +9,18 @@ def ints(line):
 
 def build_g(depth, target):
     g = {}
-    X, Y = target
+    X, Y = target[0] + 100, target[1] + 100
     for i in range(1, X + 1):
         g[i, 0] = (depth + (i * 16807)) % 20183
     for j in range(1, Y + 1):
         g[0, j] = (depth + (j * 48271)) % 20183
-    for i in range(1, X + 1):
-        for j in range(1, Y + 1):
-            g[i, j] = (depth + (g[i - 1, j] * g[i, j - 1])) % 20183
-    g[target] = depth % 20183
     g[0, 0] = depth % 20183
+    g[target] = depth % 20183
+    for j in range(1, Y + 1):
+        for i in range(1, X + 1):
+            if (i, j) == target:
+                continue
+            g[i, j] = (depth + (g[i - 1, j] * g[i, j - 1])) % 20183
     return {k: v % 3 for k, v in g.items()}
 
 
@@ -26,66 +28,30 @@ def p1(data: str):
     lines = data.splitlines()
     depth = ints(lines[0])[0]
     target = tuple(ints(lines[1]))
-
     g = build_g(depth, target)
-    return sum(g.values())
+    return sum(v for k, v in g.items() if k[0] <= target[0] and k[1] <= target[1])
 
 
 def p2(data: str):
-    # lines = data.splitlines()
-    # depth = ints(lines[0])[0]
-    # target = tuple(ints(lines[1]))
-    depth = 510
-    target=(10,10)
-    g = build_g(depth, (target[0]+20,target[1]+20))
+    lines = data.splitlines()
+    depth = ints(lines[0])[0]
+    target = tuple(ints(lines[1]))
+    g = build_g(depth, (target[0], target[1]))
     G = nx.Graph()
+
     for x, y in g:
         k0 = (x, y)
-        for i, j in [(0,1),(-1,0),(1,0),(0,-1)]:
+        # 1 torch
+        # 2 cg
+        # 0 neither
+        ta, tb = list({0, 1, 2} - {g[k0]})
+        G.add_edge((k0, ta), (k0, tb), weight=7)
+        for i, j in [(0, 1), (-1, 0), (1, 0), (0, -1)]:
             k1 = (x + i, y + j)
-            if k1 == (0, 0):
-                continue
             if k1 not in g:
                 continue
-            # 0 torch
-            # 1 cg
-            # 2 neither
-            if g[k0] == 0:
-                if g[k1] == 0:
-                    G.add_edge((k0, 0), (k1, 0), weight=1)
-                    G.add_edge((k0, 1), (k1, 1), weight=1)
-                    G.add_edge((k0, 0), (k1, 1), weight=8)
-                    G.add_edge((k0, 1), (k1, 0), weight=8)
-                elif g[k1] == 1:
-                    G.add_edge((k0, 0), (k1, 1), weight=8)
-                    G.add_edge((k0, 1), (k1, 1), weight=1)
-                else:
-                    G.add_edge((k0, 0), (k1, 0), weight=1)
-                    G.add_edge((k0, 1), (k1, 0), weight=8)
-            elif g[k0] == 1:
-                if g[k1] == 0:
-                    G.add_edge((k0, 1), (k1, 0), weight=8)
-                    G.add_edge((k0, 2), (k1, 1), weight=8)
-                elif g[k1] == 1:
-                    G.add_edge((k0, 1), (k1, 1), weight=1)
-                    G.add_edge((k0, 2), (k1, 2), weight=1)
-                    G.add_edge((k0, 1), (k1, 2), weight=8)
-                    G.add_edge((k0, 2), (k1, 1), weight=8)
-                else:
-                    G.add_edge((k0, 2), (k1, 2), weight=1)
-                    G.add_edge((k0, 1), (k1, 2), weight=8)
-            else:
-                if g[k1] == 0:
-                    G.add_edge((k0, 0), (k1, 0), weight=1)
-                    G.add_edge((k0, 2), (k1, 0), weight=8)
-                elif g[k1] == 1:
-                    G.add_edge((k0, 0), (k1, 2), weight=8)
-                    G.add_edge((k0, 2), (k1, 2), weight=1)
-                else:
-                    G.add_edge((k0, 0), (k1, 0), weight=1)
-                    G.add_edge((k0, 2), (k1, 2), weight=1)
-                    G.add_edge((k0, 0), (k1, 2), weight=8)
-                    G.add_edge((k0, 2), (k1, 0), weight=8)
-    path = nx.shortest_path(G, ((0, 0), 0), (target, 0), 'weight')
-    print(path)
+            for common in list({ta, tb}.intersection(list({0, 1, 2} - {g[k1]}))):
+                G.add_edge((k0, common), (k1, common), weight=1)
+
+    path = nx.shortest_path(G, ((0, 0), 1), (target, 1), 'weight')
     return nx.path_weight(G, path, 'weight')
