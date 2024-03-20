f49f5fab422e85ed368a39e9dd83c97a614b7c14
guohao <guohaoice@gmail.com>
2024-03-20 11:49:37 +0800

    rewrite day5-7

--- 2015/5.py
+++ 2015/5.py
@@ -2,18 +2,10 @@ import re
 
 
 def p1ss(line: str):
-    if sum(line.count(c) for c in 'aeiou') < 3:
-        return 0
-    if not re.search(r'(\w)\1', line):
-        return 0
-    if any(c in line for c in ['ab', 'cd', 'pq', 'xy']):
-        return 0
-    return 1
+    return (sum(line.count(c) for c in 'aeiou') >= 3 and re.search(r'(\w)\1', line) is not None
+            and all(c not in line for c in ['ab', 'cd', 'pq', 'xy']))
 
 
 def p2ss(line: str):
-    if not re.search(r'(\w{2}).*\1', line):
-        return 0
-    if not re.search(r'(\w)\w\1', line):
-        return 0
-    return 1
+    return (re.search(r'(\w{2}).*\1', line) is not None
+            and re.search(r'(\w)\w\1', line) is not None)
