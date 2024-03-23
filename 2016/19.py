4e07c50241b71fb94a2947fcd65afe49b2b05399
guohaoice <guohaoice@gmail.com>
2024-03-23 21:38:21 +0800

    simplify 19

--- 2016/19.py
+++ 2016/19.py
@@ -14,9 +14,9 @@ def p1(data: str):
 def p2(data: str):
     n = int(data)
     q = deque(range(1, n + 1))
-    q.rotate(len(q) // 2 + 1)
+    q.rotate(len(q) // 2)
     for i in range(n - 1):
-        if i % 2 == 0:
-            q.rotate(-1)
         q.popleft()
+        if i % 2 == 1:
+            q.rotate(-1)
     return q.popleft()
