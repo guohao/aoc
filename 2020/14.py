310ca9a1d048c1b786bc1e585c1fe5c0ea827234
guohaoice <guohaoice@gmail.com>
2024-04-29 13:04:16 +0800

    14

--- 2020/14.py
+++ 2020/14.py
@@ -2,9 +2,8 @@ import itertools
 import re
 
 
-def p2(data: str):
+def p1(data: str):
     mem = {}
-    mask = ''
     for line in data.splitlines():
         if 'mask' in line:
             mask = line.split('= ')[1]
@@ -14,13 +13,8 @@ def p2(data: str):
             r = int(r)
             l = bin(l | int(mask.replace('X', '0'), 2))[2:].zfill(36)
             xn = mask.count('X')
-            combs = set(list(itertools.combinations(xn * '01', xn)))
-            for comb in combs:
-                mi = 0
-                chs = list(l)
-                for i in range(len(l)):
-                    if mask[i] == 'X':
-                        chs[i] = comb[mi]
-                        mi += 1
+            for comb in set(list(itertools.combinations(xn * '01', xn))):
+                comb = iter(comb)
+                chs = [l[i] if mask[i] != 'X' else next(comb) for i in range(len(l))]
                 mem[int(''.join(chs), 2)] = r
     return sum(mem.values())
