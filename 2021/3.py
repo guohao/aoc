7bbd88831b8e02a8c41c72fe6c6c56a84757cd5a
guohao <guohaoice@gmail.com>
2024-05-05 12:10:19 +0800

    refactor 3

--- 2021/3.py
+++ 2021/3.py
@@ -12,30 +12,16 @@ def p1(data: str):
 
 
 def p2(data: str):
-    ns = data.splitlines()
-    mcs = ns.copy()
-    mc = 0
-    for i in range(len(ns[0])):
-        if len(mcs) == 1:
-            mc = mcs[0]
-            break
-        a = list(zip(*mcs))
-        c = Counter(a[i])
-        if c['1'] == c['0']:
-            bmc = '1'
-        else:
-            bmc = c.most_common()[0][0]
-        mcs = [x for x in mcs if x[i] == bmc]
-    lcs = ns.copy()
-    lc = 0
-    for i in range(len(ns[0])):
-        if len(lcs) == 1:
-            lc = lcs[0]
-            break
-        c = Counter(list(zip(*lcs))[i])
-        if c['1'] == c['0']:
-            blc = '0'
-        else:
-            blc = c.most_common()[-1][0]
-        lcs = [x for x in lcs if x[i] == blc]
-    return int(lc, 2) * int(mc, 2)
+    def rating(most_common: bool):
+        remains = data.splitlines()
+        for i in range(len(remains[0])):
+            if len(remains) == 1:
+                return int(remains[0], 2)
+            c = Counter(list(zip(*remains))[i])
+            if c['1'] == c['0']:
+                pick = '1' if most_common else '0'
+            else:
+                pick = c.most_common()[not most_common][0]
+            remains = [x for x in remains if x[i] == pick]
+
+    return rating(True) * rating(False)
