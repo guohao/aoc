4548c318c2e4dd80d2f25dd9ba8f32702c32a7a7
guohaoice <guohaoice@gmail.com>
2024-05-06 10:04:55 +0800

    day4

---
+++ 2021/4.py
@@ -0,0 +1,40 @@
+def solve(data: str):
+    parts = data.split('\n\n')
+    ns = list(map(int, parts[0].split(',')))
+    gs = []
+    for part in parts[1:]:
+        g = {}
+        gv = {}
+        for i, line in enumerate(part.splitlines()):
+            for j, c in enumerate(line.split()):
+                g[int(c)] = i, j
+                gv[i, j] = int(c)
+        gs.append((g, gv, set()))
+    wins = []
+    for n in ns:
+        ngs = []
+        for i in range(len(gs)):
+            g, gv, v = gs[i]
+            if n not in g:
+                ngs.append((g, gv, v))
+                continue
+            x, y = g[n]
+            v.add(n)
+            r = range(int(len(g) ** 0.5))
+            if all(gv[x, j] in v for j in r):
+                wins.append((sum(g) - sum(v)) * n)
+                continue
+            if all(gv[j, y] in v for j in r):
+                wins.append((sum(g) - sum(v)) * n)
+                continue
+            ngs.append((g, gv, v))
+        gs = ngs
+    return wins
+
+
+def p1(data: str):
+    return solve(data)[0]
+
+
+def p2(data: str):
+    return solve(data)[-1]
