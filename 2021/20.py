477a1c228427375e745a52e693a2360e498a92a4
guohaoice <guohaoice@gmail.com>
2024-05-18 11:59:08 +0800

    day20

--- 2021/20.py
+++ 2021/20.py
@@ -1,41 +1,40 @@
 import itertools
+from functools import cache
 
 import myutil
 
 
-def p1(data: str):
+def solve(data: str, t):
     parts = data.split('\n\n')
     iea = parts[0].replace('\n', '')
     assert len(iea) == 512
     g = myutil.d2g(parts[1])
+
+    @cache
+    def st(ix, iy, it):
+        if it == 0:
+            return v(ix, iy)
+        bs = ''.join(
+            str(int(st(ix + kx, iy + ky, it - 1) == '#')) for kx, ky in itertools.product(range(-1, 2), repeat=2))
+        return iea[int(bs, 2)]
 
     def v(ix, iy):
         if (ix, iy) in g:
             return g[ix, iy]
         return '.'
 
-    for _ in range(2):
-        ng = {}
-        (min_x, max_x), (min_y, max_y) = myutil.range_of_grid_2(g)
-        for i in range(min_x, max_x + 1):
-            for j in range(min_y, max_y + 1):
-                bs = ''.join(str(int(v(i + kx, j + ky) == '#')) for kx, ky in itertools.product(range(-1, 2), repeat=2))
-                idx = int(bs, 2)
-                ng[i, j] = iea[idx]
-        g = ng
-    return sum(x == '#' for x in g.values())
+    (min_x, max_x), (min_y, max_y) = myutil.range_of_grid_2(g)
+    ans = 0
+    for i in range(min_x - 2 * t, max_x + 2 * t):
+        for j in range(min_y - 2 * t, max_y + 2 * t):
+            if st(i, j, t) == '#':
+                ans += 1
+    return ans
 
-# print(p1("""..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..##
-# #..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###
-# .######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#.
-# .#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#.....
-# .#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#..
-# ...####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.....
-# ..##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
-#
-# #..#.
-# #....
-# ##..#
-# ..#..
-# ..###
-# """))
+
+def p1(data: str):
+    return solve(data, 2)
+
+
+def p2(data: str):
+    return solve(data, 50)
