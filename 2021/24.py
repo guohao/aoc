ad4caa8ad9f480ecaf2210d0b2d46237d5c1f9a3
guohao <guohaoice@gmail.com>
2024-05-22 17:10:45 +0800

    24

--- 2021/24.py
+++ 2021/24.py
@@ -1,127 +1,50 @@
-import re
-from sympy import symbols, simplify, sympify
+from z3 import *
+
+
+def solve(data: str, goal):
+    s = Optimize()
+    size = int(math.log2(10 ** 14)) + 1
+    digits = [BitVec(f'd_{i}', size) for i in range(14)]
+    var = {r: 0 for r in 'xyzw'}
+
+    pos = 0
+    for i, inst in enumerate(data.splitlines()):
+        instr, a_name, *b = inst.split()
+        a = var[a_name]
+        if b:
+            b = var[b[0]] if b[0] in var else int(b[0])
+        c = BitVec(f'v_{i}', size)
+
+        match instr:
+            case 'inp':
+                c = digits[pos]
+                pos += 1
+            case 'add':
+                s.add(c == a + b)
+            case 'mul':
+                s.add(c == a * b)
+            case 'mod':
+                s.add(c == a % b)
+            case 'div':
+                s.add(c == a / b)
+            case 'eql':
+                s.add(c == If(a == b, BitVecVal(0, size), BitVecVal(1, size)))
+        var[a_name] = c
+
+    s.add([0 < d for d in digits])
+    s.add([d < 10 for d in digits])
+    s.add(var['z'] == 0)
+
+    optimizer = [s.maximize, s.minimize][goal]
+    optimizer(sum((10 ** i) * d for i, d in enumerate(digits[::-1])))
+    s.check()
+    m = s.model()
+    return ''.join([str(m[d]) for d in digits])
 
 
 def p1(data: str):
-    def is_num(name: str):
-        return re.fullmatch(r'-?\d+', name)
+    return solve(data, 0)
 
-    #
-    #     def valid2(sn: str):
-    #         for line in data.strip().splitlines():
-    #             c = line.split()[0]
-    #             a = line.split()[1]
-    #             assert a in 'wxyz'
-    #             if len(line.split()) == 3:
-    #                 b = line.split()[2]
-    #             match c:
-    #                 case "inp":
-    #                     rs[a] = int(next(itr))
-    #                 case "add":
-    #                     rs[a] = vo(a) + vo(b)
-    #                 case "mul":
-    #                     rs[a] = vo(a) * vo(b)
-    #                 case "div":
-    #                     rs[a] = vo(a) // vo(b)
-    #                 case "mod":
-    #                     rs[a] = vo(a) % vo(b)
-    #                 case "mod":
-    #                     rs[a] = vo(a) % vo(b)
-    #                 case "eql":
-    #                     rs[a] = int(vo(a) == vo(b))
-    #                 case _:
-    #                     raise Exception()
-    #         return rs['z'] == 0
 
-    def parse():
-        def co(name: str):
-            if is_num(name):
-                return int(name)
-            return rs[name]
-
-        i = 0
-        des = []
-        for part in data.split('inp w\n'):
-            if not part:
-                continue
-            rs = {x: f'pre{x}' for x in 'wxyz'}
-            rs['w'] = f's{i}'
-            i += 1
-            for line in part.splitlines():
-                c = line.split()[0]
-                a = line.split()[1]
-                assert a in 'wxyz'
-                if len(line.split()) == 3:
-                    b = line.split()[2]
-                match c:
-                    case "inp":
-                        rs[a] = f'int(s[{i}])'
-                        i += 1
-                    case "add":
-                        rs[a] = f'({rs[a]} + {co(b)})'
-                    case "mul":
-                        rs[a] = f'({rs[a]} * {co(b)})'
-                    case "div":
-                        rs[a] = f'({rs[a]} // {co(b)})'
-                    case "mod":
-                        rs[a] = f'({rs[a]} % {co(b)})'
-                    case "eql":
-                        rs[a] = f'(1-abs({rs[a]} - {co(b)}))'
-                    case _:
-                        raise Exception()
-            des.append(rs['z'])
-        return des
-
-    des = parse()
-
-    parsed = []
-    # sbs = ' '.join(f'pre{k}' for k in 'xyzw')
-    # sbs +=' '
-    # sbs += ' '.join(k for k in 'xyzw')
-    # sbs +=' '
-    # sbs += ' '.join(f's{k}' for k in range(14))
-    zs = symbols("prez")
-    zss = [0, -18, 7598, -3411808]
-    ss = symbols('s:14')
-    prez = {zs: sympify('0')}
-    combs = []
-    for i, es in enumerate(des):
-        d = {}
-        exp = sympify(es)
-        # exp = exp.subs(prez)
-        exp = simplify(exp)
-        for j in range(1, 10):
-            tmpe = exp.subs({ss[i]: j, zs: zss[i]})
-            d[j] = tmpe.evalf()
-        prez = {zs: exp}
-        print(prez)
-    # print(prev)
-    assert len(des) == 14
-
-    for n in range(int('9' * 14), int('1' * 14), -1):
-        print(n)
-        sbs = ''
-
-        # x, y = symbols(sbs)
-        s = str(n)
-        if '0' in s:
-            continue
-        x = 'x'
-        y = 'y'
-        z = 'z'
-        pre = {k: 0 for k in 'wxyz'}
-        for exe in des:
-            # print(exe)
-            mem = {'s': s, 'pre': pre, 'x': x, 'y': y, 'z': z}
-            np = {}
-            for k, v in exe.items():
-                # print(v)
-
-                print(simp)
-                # print(pre)
-                # print(k,v)
-                # np[k] = int(eval(v, mem))
-            pre = np
-        break
-        if pre['z'] == 0:
-            return n
+def p2(data: str):
+    return solve(data, 1)
