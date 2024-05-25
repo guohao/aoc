a40deb9abaa40600386cc7853380727226874b8d
guohaoice <guohaoice@gmail.com>
2024-05-26 02:05:21 +0800

    6-7

--- 2023/6.py
+++ 2023/6.py
@@ -2,17 +2,11 @@ from util import ints
 
 
 def p1(data: str):
-    lines = data.splitlines()
-    t = ints(lines[0])
-    d = ints(lines[1])
     ans = 1
-    for i in range(len(t)):
-        ans *= sum((t[i] - j) * j > d[i] for j in range(t[i]))
+    for t, d in zip(*map(ints, data.splitlines())):
+        ans *= sum((t - j) * j > d for j in range(t))
     return ans
 
 
 def p2(data: str):
-    lines = data.replace(' ', '').splitlines()
-    t = ints(lines[0])[0]
-    d = ints(lines[1])[0]
-    return sum((t - j) * j > d for j in range(t))
+    return p1(data.replace(' ', ''))
