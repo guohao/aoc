b08f8ae1196b10afb075dcfb1e9288dc027f25b7
gh <guohaoice@gmail.com>
2024-11-21 19:22:22 +0800

    2016 day2

--- 2016/day2/part2.py
+++ 2016/day2/part2.py
@@ -1,26 +1,33 @@
-x = y = 0
 ans = ''
 import sys
 
 lines = [line.strip() for line in sys.stdin.readlines()]
+g = {}
+k = 1
+i = j = 2
+while i >= -2:
+    g[i, j] = hex(k)[2:].upper()
+    k += 1
+    if abs(i) + abs(j) == 4:
+        i -= 1
+        j = abs(i)
+    else:
+        j += 1
+
+x = y = 0
 for line in lines:
     for c in line:
+        px, py = x, y
         if c == 'R':
             y += 1
         elif c == 'L':
             y -= 1
         elif c == 'U':
-            x -= 1
-        else:
             x += 1
-        x = max(-1, min(1, x))
-        y = max(-1, min(1, y))
-    g = {}
-    k = 1
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            g[i, j] = k
-            k += 1
-    ans += str(g[x, y])
+        else:
+            x -= 1
+        if (x, y) not in g:
+            x, y = px, py
+    ans += g[x, y]
 
 print(ans)
