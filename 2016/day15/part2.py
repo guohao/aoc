42c754d0274b1e1b2345e96b4e012bd2443088d7
guohaoice <guohaoice@gmail.com>
2024-11-23 01:32:25 +0800

    2016 15

---
+++ 2016/day15/part2.py
@@ -0,0 +1,15 @@
+import itertools
+import sys
+import re
+
+lines = [line.strip() for line in sys.stdin.readlines()]
+ans = 1
+ps = []
+for line in lines:
+    nums = list(map(int, re.findall(r'-?\d+', line)))
+    ps.append((nums[1], nums[-1]))
+ps.append((11,0))
+for i in itertools.count(0):
+    if all((i + b + j) % a == 0 for j, (a, b) in enumerate(ps, start=1)):
+        print(i)
+        break
