b66ab348bbb475c3315c54bc463d3f25191f66f5
guohaoice <guohaoice@gmail.com>
2024-12-01 13:28:02 +0800

    2024 day1

--- 2024/day1/part2.py
+++ 2024/day1/part2.py
@@ -4,4 +4,4 @@ from collections import Counter
 
 a, b = zip(*[list(map(int, re.findall(r'-?\d+', l))) for l in sys.stdin.readlines()])
 b = Counter(b)
-print(sum((a[i] * b[a[i]]) for i in range(len(a))))
+print(sum((x * b[x]) for x in a))
