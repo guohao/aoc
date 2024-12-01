7caa43e777604e0b4f42d100473be606c9dbbcd2
guohaoice <guohaoice@gmail.com>
2024-12-01 13:27:11 +0800

    2024 day1

--- 2024/day1/part2.py
+++ 2024/day1/part2.py
@@ -2,9 +2,6 @@ import re
 import sys
 from collections import Counter
 
-ns = [list(map(int, re.findall(r'-?\d+', l))) for l in sys.stdin.readlines()]
-
-a = [n[0] for n in ns]
-b = [n[1] for n in ns]
+a, b = zip(*[list(map(int, re.findall(r'-?\d+', l))) for l in sys.stdin.readlines()])
 b = Counter(b)
 print(sum((a[i] * b[a[i]]) for i in range(len(a))))
