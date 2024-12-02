26e19f718a1e1535acbb018d5461ce1fe58970d6
gh <guohaoice@gmail.com>
2024-12-02 18:37:27 +0800

    2022 day4

---
+++ 2022/day4/part2.py
@@ -0,0 +1,9 @@
+import re
+import sys
+
+ls = [line.strip() for line in sys.stdin.readlines()]
+t = 0
+for line in ls:
+    a, b, c, d = list(map(int, re.findall(r'\d+', line)))
+    t += (d - a) * (c - b) <= 0
+print(t)
