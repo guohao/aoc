f3909ce0b7fced6452f8e0248ed9f1e2bf8fc397
gh <guohaoice@gmail.com>
2024-12-03 14:00:06 +0800

    2024 day3

--- 2024/day3/part2.py
+++ 2024/day3/part2.py
@@ -1,19 +1,17 @@
-import sys
 import re
+import sys
 
-lines = sys.stdin.readlines()
-lines = [line.strip() for line in lines]
+s = sys.stdin.read()
 t = 0
 
 e = True
-for line in lines:
-    for m in re.finditer(r"do\(\)|don't\(\)|mul\((\d+),(\d+)\)", line):
-        if 'don' in m.group():
-            e = False
-        elif 'do' in m.group():
-            e = True
-        else:
-            if e:
-                t += int(m.group(1)) * int(m.group(2))
+for m in re.finditer(r"do\(\)|don't\(\)|mul\((\d+),(\d+)\)", s):
+    if 'don' in m.group():
+        e = False
+    elif 'do' in m.group():
+        e = True
+    else:
+        if e:
+            t += int(m.group(1)) * int(m.group(2))
 
 print(t)
