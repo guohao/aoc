97cf50a8ebb5c8462568623b0c9ec98870f164f9
guohaoice <guohaoice@gmail.com>
2024-12-10 13:11:51 +0800

    2024 day10

---
+++ 2024/day10/part1.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque, defaultdict
+
+ls = [l.strip() for l in sys.stdin.readlines()]
+g = defaultdict(str) | {(i, j): ls[i][j] for i in range(len(ls)) for j in range(len(ls[i]))}
+t = 0
+
+q = deque()
+starts = [x for x in g if g[x] == '0']
+for s in starts:
+    q.append((s, s))
+seen = set()
+while q:
+    s, p = q.popleft()
+    if g[p] == '9':
+        seen.add((s,p))
+        continue
+    x, y = p
+    for dx, dy in [(1, 0), (0, 1), (0, -1), (-1, 0)]:
+        nx, ny = x + dx, y + dy
+        if g[nx, ny] == str(int(g[p]) + 1):
+            q.append((s, (nx, ny)))
+
+print(len(seen))
