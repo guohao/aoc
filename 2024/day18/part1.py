b850264c2247c95081e10dd392a3639ee2fadf3b
guohaoice <guohaoice@gmail.com>
2024-12-19 01:29:14 +0800

    simplify

--- 2024/day18/part1.py
+++ 2024/day18/part1.py
@@ -6,4 +6,4 @@ import networkx as nx
 G = nx.grid_2d_graph(71, 71)
 for line in sys.stdin.readlines()[:1024]:
     G.remove_node(tuple(map(int, re.findall(r'-?\d+', line))))
-print(nx.shortest_path_length(G, (0, 0), (70, 70)))
+print(nx.shortest_path_length(G, min(G), max(G)))
--- 2024/day18/part2.py
+++ 2024/day18/part2.py
@@ -7,7 +7,7 @@ G = nx.grid_2d_graph(71, 71)
 for line in sys.stdin.readlines():
     G.remove_node(tuple(map(int, re.findall(r'-?\d+', line))))
     try:
-        nx.shortest_path_length(G, (0, 0), (70, 70))
+        nx.shortest_path_length(G, min(G), max(G))
     except:
         print(line)
         break
