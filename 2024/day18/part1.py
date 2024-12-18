7a0b5d987db5fdf723c2bd7cb7965da82549fb3f
guohaoice <guohaoice@gmail.com>
2024-12-19 01:27:21 +0800

    simplify

--- 2024/day18/part1.py
+++ 2024/day18/part1.py
@@ -1,17 +1,9 @@
-import itertools
 import re
 import sys
 
 import networkx as nx
 
-G = nx.Graph()
-for i, j in itertools.product(range(71), repeat=2):
-    k = i, j
-    for dx, dy in [(1, 0), (0, 1), (0, -1), (-1, 0)]:
-        nbx, nby = i + dx, j + dy
-        if 0 <= nbx <= 70 and 0 <= nby <= 70:
-            G.add_edge(k, (nbx, nby))
+G = nx.grid_2d_graph(71, 71)
 for line in sys.stdin.readlines()[:1024]:
-    x, y = list(map(int, re.findall(r'-?\d+', line)))
-    G.remove_node((x, y))
+    G.remove_node(tuple(map(int, re.findall(r'-?\d+', line))))
 print(nx.shortest_path_length(G, (0, 0), (70, 70)))
